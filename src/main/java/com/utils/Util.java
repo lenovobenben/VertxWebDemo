a535abc56cc428ce3b20bad5d93cc0f671442dcd
lihd <lihd@mail.jj.cn>
2018-01-29 20:11:23 +0800

    。。。

--- src/main/java/com/utils/Util.java
+++ src/main/java/com/utils/Util.java
@@ -57,22 +57,20 @@ public class Util {
 
         logger.info("response : " + rc.get(Const.RESP));
 
-        if (rc.get(Const.START) != null) {
-            long now = System.currentTimeMillis();
-
-            long consume = now - (Long)rc.get(Const.START);
-            if (consume > Const.WARN_TIME) {// handler 耗时预警
-                logger.warn("url :  " + rc.request().absoluteURI()
-                        + "   param : " + rc.getBodyAsString()
-                        + "   consume : " + consume);
-            }
-        }
+        longTimeConsumingLog(rc);
 
         rc.response().end((String)rc.get(Const.RESP));
     }
 
     public static void handlerFail (RoutingContext rc, int status) {
 
+        longTimeConsumingLog(rc);
+
+        rc.fail(status);
+    }
+
+
+    private static void longTimeConsumingLog(RoutingContext rc) {
         if (rc.get(Const.START) != null) {
             long now = System.currentTimeMillis();
 
@@ -83,7 +81,5 @@ public class Util {
                         + "   consume : " + consume);
             }
         }
-
-        rc.fail(status);
     }
 }
